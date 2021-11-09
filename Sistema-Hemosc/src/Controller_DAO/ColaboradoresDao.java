dc2b90fe2378cfce160540cb4b1f7e2aebca538c
Gabriel <gabrielrodrigues2643@gmail.com>
2021-11-09 16:19:15 -0300

    criação de listar e pesquisar em ColaboradoresDao

--- Sistema-Hemosc/src/Controller_DAO/ColaboradoresDao.java
+++ Sistema-Hemosc/src/Controller_DAO/ColaboradoresDao.java
@@ -12,7 +12,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 import Model.Colaboradores;
-import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
@@ -24,32 +23,60 @@ import java.util.logging.Logger;
  */
 public class ColaboradoresDao {
     
-    public void cadastrar(Colaboradores c) {
-
+    public List<Colaboradores > listar(){
         Connection conn = ConexaoBanco.conectaBanco();
-        
         PreparedStatement stmt = null;
         ResultSet rs = null;
+        List<Colaboradores> Colaboradores = new ArrayList<>();
+        
+        try {
+            stmt = conn.prepareStatement("SELECT * FROM colaboradores");
+            
+            rs = stmt.executeQuery();
+        while (rs.next()) {
+            Colaboradores c = new Colaboradores();
+            c.setId_colaborador(rs.getInt("id_colaborador"));
+            c.setId_cidade(rs.getInt("id_cidade"));
+            c.setNome(rs.getString("nome"));
+            c.setEndereco(rs.getString("endereco"));
+            c.setFuncao(rs.getString("funcao"));
+           
+     
+            Colaboradores.add(c);
+            
+        }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(ColaboradoresDao.class.getName()).log(Level.SEVERE, null, ex);
+        
+        }
+        return Colaboradores;
+        }
+    
+    
+    public void alterar(Colaboradores c) {
 
-       try{
-       stmt = conn.prepareStatement("INSERT INTO colaborador (id_cidade,nome,endereco,funcao) VALUES (?,?,?,?)");
-       stmt.setInt(1,c.getId_cidade());
-       stmt.setString(2,c.getNome());
-       stmt.setString(3,c.getEndereco());
-       stmt.setString(4,c.getFuncao());
-       
-       
-       stmt.executeUpdate();
-       
-           JOptionPane.showMessageDialog(null,"Colaborador cadastrado com sucesso!");
-       
-       } catch (SQLException ex) {
-       
-       }
-    
-    
+        Connection conn = ConexaoBanco.conectaBanco();
+
+        PreparedStatement stmt = null;
+
+        
+        try {
+            stmt = conn.prepareStatement("UPDATE colaboradores set  id_cidade = ?, nome = ?, endereco = ?, funcao = ? where id_colaborador = ?");
+            stmt.setInt(1, c.getId_cidade());
+            stmt.setString(2, c.getNome());
+            stmt.setString(3, c.getEndereco());
+            stmt.setString(4, c.getFuncao());            
+            stmt.setInt(5, c.getId_colaborador());
+
+            stmt.executeUpdate();
+
+            JOptionPane.showMessageDialog(null, "Colaborador alterado com Sucesso!");
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(ColaboradoresDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
     
     
 }
